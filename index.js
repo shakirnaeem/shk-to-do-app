79e0fb3a392aa963061f9a45b531b08a9b950e5b
Shakir Naeem <shakir.naeem@synergy-it.com>
2020-10-25 16:20:47 +0500

    change port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // create express app
 const app = express();
+const port = process.env.port || 3000;
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,6 +16,6 @@ app.get('/', (req, res) => {
 });
 
 //listen for requests
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is listening on port 3000");
 });
