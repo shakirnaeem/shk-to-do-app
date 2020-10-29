00ba098e35b1a8edc4978b0f5a09ca06e48e748b
Shakir Naeem <shakir.naeem@synergy-it.com>
2020-10-29 11:14:36 +0500

    s

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-mongoose.Promise = global.Promise;
-//Connecting to the database
-mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database"); 
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+// mongoose.Promise = global.Promise;
+// //Connecting to the database
+// mongoose.connect(process.env.DB_URL, {
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log("Successfully connected to the database"); 
+// }).catch(err => {
+//     console.log('Could not connect to the database. Exiting now...', err);
+//     process.exit();
+// });
 
 // define a simple route
 app.get('/', (req, res) => {
